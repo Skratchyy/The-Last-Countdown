504ea7e890381d9af00ebeab23ae00bf013f5c9f
Skratchyy <skratchy@MacBook-Air.local>
2023-11-01 18:38:44 -0700

    The last countdown

---
+++ src/components/ResultModal.jsx
@@ -0,0 +1,41 @@
+import { forwardRef, useRef, useImperativeHandle } from "react";
+import { createPortal } from "react-dom";
+const ResultModal = forwardRef(function ResultModal(
+  { targetTime, remainingTime, onReset },
+  ref
+) {
+  const dialog = useRef();
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const userLost = remainingTime <= 0;
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  });
+
+  return createPortal(
+    <dialog ref={dialog} className="result-modal">
+      {userLost && <h2>You Lost</h2>}
+      {!userLost && <h2>Your score: {score}!</h2>}
+      <p>
+        Target time was{" "}
+        <strong>
+          {targetTime} second {targetTime > 1 ? "s" : ""}
+        </strong>
+      </p>
+      <p>
+        You stopped timer with
+        <strong> {formattedRemainingTime} seconds left.</strong>
+      </p>
+      <form method="dialog" onSubmit={onReset}>
+        <button>Close</button>
+      </form>
+    </dialog>,
+    document.getElementById("modal")
+  );
+});
+
+export default ResultModal;
